31f0ac3877bb3698caafc73c5c24a689bd548c01
banzhijiangshan <2100012991@stu.pku.edu.cn>
2023-10-09 22:37:11 +0800

    demo

---
+++ vue/src/components/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="sign-up">
+    <img src="../assets/2.jpg" alt="Sign up" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style scoped>
+.sign-up {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black; /* 可选，设置背景色 */
+}
+
+.sign-up img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain; /* 修改为contain以保持图片比例且充满容器 */
+}
+</style>
